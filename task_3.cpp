af5d98d348c8c6733869fe2aa3ed87d633a12ea7
prostto_vika <vikabonchkovskaya@gmail.com>
2025-09-15 17:14:16 +0300

    неправильное возвращение функции

--- task_3.cpp
+++ task_3.cpp
@@ -16,5 +16,5 @@ double y = sin(x); // функция y = sin(x)
 std::cout << x << " | " << y << std::endl; // Выводим значения 
 функции для каждого аргумента (x | y) 
 } 
-return nullptr; 
+return 0; 
 } 
